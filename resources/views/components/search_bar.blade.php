a7520fb6f3535defda2899e54f2480bb7e54eda8
EzraArya <ezrawijaya10@gmail.com>
2024-05-28 16:38:55 +0700

    refactor: refactor search bar into its own component

---
+++ resources/views/components/search_bar.blade.php
@@ -0,0 +1,6 @@
+<div class="search_bar">
+    <form class="d-flex gap-1" role="search">
+        <input class="flex-grow w-[500px] border border-1 border-grey-100 text-center" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </form>
+</div>
