93c9b7c9e3bd5aee0d56752759e796b4a98f0f3f
EzraArya <ezrawijaya10@gmail.com>
2024-05-30 10:04:47 +0700

    refactor: refactor home page to extend template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="https://kit.fontawesome.com/5a8ab06af1.js" crossorigin="anonymous"></script>
-    @vite('resources/css/app.css')
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+@extends('layout.template')
 
-</head>
-<body>
-    @include('components.header')
-    
-</body>
-</html>
+@section('content')
+    <div class="mt-4">
+        @include('components.carousel')
+    </div>
+    <div class="mt-4">
+        @include('components.announcement_bar')
+        
+    </div>
+    <div class="mt-4">
+        @include('components.bento')
+    </div>
+    <div class="mt-4">
+        @include('components.recomendation_bar')
+    </div>
+@endsection
