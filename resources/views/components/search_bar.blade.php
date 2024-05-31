34e9cbb8599aa7c538ee3105b1e84557be5ceba1
EzraArya <ezrawijaya10@gmail.com>
2024-05-31 21:19:29 +0700

    style(search_bar): changed border color for consistency

--- resources/views/components/search_bar.blade.php
+++ resources/views/components/search_bar.blade.php
@@ -1,6 +1,7 @@
 <div class="search_bar">
     <form class="d-flex gap-1" role="search">
+        <x-dropdown_btn></x-dropdown_btn>
         <input class="flex-grow w-[500px] border border-1 border-grey-100 text-center" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+        <button class="btn border border-blue-500 hover:bg-blue-500 hover:text-white-100    " type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
     </form>
 </div>
