13603ef0bbfc7d4dabb02e45caa98565383a523c
EzraArya <ezrawijaya10@gmail.com>
2024-05-31 21:18:12 +0700

    feat(product.blade.php): added highest bidder section

--- resources/views/sections/Products/product.blade.php
+++ resources/views/sections/Products/product.blade.php
@@ -73,6 +73,11 @@
                     </div>
                     <hr class="w-full mt-2">
                     <div class="flex justify-between w-full">
+                        <h1 class="font-montserrat text-sm">Highest Bidder</h1>
+                        <h1 class="font-montserrat text-sm">X</h1>
+                    </div>
+                    <hr class="w-full mt-2">
+                    <div class="flex justify-between w-full">
                         <h1 class="font-montserrat text-sm">Opening Time</h1>
                         <h1 class="font-montserrat text-sm">X</h1>
                     </div>
@@ -181,11 +186,11 @@
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <div class="col-span-2">
                         <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                        <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
+                        <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="$2999" required="">
                     </div>
                 </div>
                 <button type="submit" class="w-full text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Add new product
+                    Bid
                 </button>
             </form>
         </div>
