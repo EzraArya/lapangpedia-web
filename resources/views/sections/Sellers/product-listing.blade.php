75cfb941a92fcaf897659fe12551a1e9b69de601
EzraArya <ezrawijaya10@gmail.com>
2024-06-02 20:12:56 +0700

    feat(product-listing.blade.php): Connect form with the database using POST method

--- resources/views/sections/Sellers/product-listing.blade.php
+++ resources/views/sections/Sellers/product-listing.blade.php
@@ -1,10 +1,16 @@
 @extends('layout.template')
 @section('content')
+@if (session('message'))
+    <div class="alert alert-success">
+        {{ session('message') }}
+    </div>
+@endif
     <section class="bg-white">
         <div class="py-4 px-2 mx-auto max-w-screen-xl sm:py-4 lg:px-6 m-4">
             <h1 class="text-lg bg-blue-500 font-medium text-white-100 rounded-tr-md text-md rounded-tl-md font-montserrat indent-3 mb-1">Product Details</h1>
             <div class="container h-full w-full flex flex-col justify-center items-center gap-4">
-                <form action="" class="justify-center flex flex-col items-center w-[30rem]">
+                <form action="{{route('store-product')}}" enctype="multipart/form-data" method="POST" class="justify-center flex flex-col items-center w-[30rem]">
+                    @csrf
                     <h1 class="font-montserrat font-bold text-4xl text-blue-500 mb-2">Product Details</h1>
                     <hr class="w-full h-2 mb-2">
                     <div class="container flex flex-col items-center mb-2">
@@ -17,8 +23,16 @@
                         <input type="text" name="name" class="border border-grey-100 focus:border-black rounded-md h-10 w-full indent-2 font-montserrat text-sm" placeholder="">
                     </div>
                     <div class="container flex flex-col items-center mb-2">
-                        <label for="details" class="font-montserrat font-semibold text-lg text-grey-100 text-left self-start">Product Details</label>
-                        <input type="text" name="details" class="border border-grey-100 focus:border-black rounded-md h-9 w-full indent-2 font-montserrat text-sm" placeholder="">
+                        <label for="description" class="font-montserrat font-semibold text-lg text-grey-100 text-left self-start">Product Description</label>
+                        <input type="text" name="description" class="border border-grey-100 focus:border-black rounded-md h-9 w-full indent-2 font-montserrat text-sm" placeholder="">
+                    </div>
+                    <div class="container flex flex-col items-center mb-2">
+                        <label for="category_id" class="font-montserrat font-semibold text-lg text-grey-100 text-left self-start">Product Category</label>
+                        <select name="category_id" class="border border-grey-100 focus:border-black rounded-md h-9 w-full indent-2 font-montserrat text-sm">
+                            @foreach($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
                     <div class="container flex flex-col items-center mb-2">
                         <label for="condition" class="font-montserrat font-semibold text-lg text-grey-100 text-left self-start">Item Condition</label>
