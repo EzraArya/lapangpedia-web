163290256a3a542918310033c5e8bafad296971d
EzraArya <ezrawijaya10@gmail.com>
2024-06-05 18:04:28 +0700

    chore(BidController): moved placeBid function to BidController to better fit context

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Auction;
+use App\Models\Bid;
 use Illuminate\Http\Request;
 
 class BidController extends Controller
 {
-    //
+    public function placeBid(Request $request, Auction $auction)
+    {
+        $request->validate([
+            'price' => 'required|numeric|min:' . ($auction->highestBid->price ?? $auction->starting_price)
+        ]);
+    
+        $bid = Bid::create([
+            'price' => $request->price,
+            'user_id' => auth()->id(),
+            'auction_id' => $auction->id,
+        ]);
+    
+        return redirect()->back()->with('success', 'Your bid has been placed successfully!');
+    }
 }
