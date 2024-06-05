727cd2b86f9e07e1d9dd6f4e83a319e034ee7589
EzraArya <ezrawijaya10@gmail.com>
2024-06-05 18:03:37 +0700

    feat (AuctionController.php): add function to buyout auction and end auction by time

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -10,15 +10,34 @@ use Illuminate\Support\Facades\DB;
 
 class AuctionController extends Controller
 {
-    public function endAuction(Auction $auction)
-    {
-        if ($auction->status == 'ongoing' && ($auction->isBoughtOut() || $auction->hasTimeEnded())) {
-            $auction->status = 'ended';
+    public function buyoutProduct(Request $request, Auction $auction){
+        $request->validate([
+            'agree' => 'required',
+        ]);
+        if ($auction->auction_status == "ongoing") {
+            // return redirect()->route('home-page');
+            $auction->auction_status = 'ended';
+            $auction->winner_id = Auth::id();
             $auction->save();
     
-            return redirect()->back()->with('success', 'The auction has ended successfully!');
+            return redirect()->back()->with('success', 'The product has been bought out successfully!');
         }
     
-        return redirect()->back()->with('error', 'The auction could not be ended.');
+        return redirect()->withErrors([
+            'agree.reqired' => 'You must agree to the terms and conditions.'
+        ]);
     }
+
+    public function endAuctionByTime(Auction $auction){
+        if (now() > $auction->end_time) {
+            $auction->auction_status = 'ended';
+            $auction->winner_id = $auction->highestBid();
+            $auction->save();
+    
+            return redirect()->back()->with('success', 'The auction has ended due to time expiry.');
+        }
+    
+        return redirect()->back()->with('error', 'The auction is still ongoing.');
+    }
+
 }
