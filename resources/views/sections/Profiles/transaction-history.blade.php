340031d0eb288f1b36082f8ba1154be8a9f24248
EzraArya <ezrawijaya10@gmail.com>
2024-06-05 18:08:31 +0700

    feat: add foreach function to display product based on its status

--- resources/views/sections/Profiles/transaction-history.blade.php
+++ resources/views/sections/Profiles/transaction-history.blade.php
@@ -6,24 +6,27 @@
         <div class="mt-4 w-full">
             <div id="tabs-1" class="tab-content w-full flex flex-warp flex-col gap-2  mt-4">
                 <div class="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white max-h-[30rem] overflow-auto gap-4">
+                @foreach ($auctions as $auction)
+                    @if ($auction->payment_status == "paid" && $auction->auction_status == "ended")  
                     <div class="m-2">
-                        @include('components.hr_card')
+                        <div class="hr_card">
+                            <a href="{{route('product-page', ['id' => $auction->product->id])}}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                @if ($auction->product->images->count() > 0)
+                                <img class="rounded-t-lg h-52 w-64 object-cover" src="{{ asset('storage/' . $auction->product->images->first()->path) }}" alt="" />
+                                @endif
+                                <div class="flex flex-col justify-between p-4 leading-normal w-full items-center">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$auction->product->name}}</h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$auction->product->description}}</p>
+                                    <div class="w-full flex justify-evenly items-center h-full">
+                                        <p class="mb-3 font-normal text-lg text-blue-500 dark:text-blue-500">{{$auction->product->starting_price}}</p>
+                                        <p class="mb-3 font-normal text-lg text-red-800">{{\Carbon\Carbon::parse($auction->start_time)->diffForHumans() }}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>                    
                     </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
+                    @endif                    
+                @endforeach
                 </div>
             </div>
         </div>
--- resources/views/sections/Transactions/cart.blade.php
+++ resources/views/sections/Transactions/cart.blade.php
@@ -18,49 +18,55 @@
             </div>
         </div>
         <div id="tabs-1" class="tab-content hidden w-full flex flex-warp flex-col gap-2  mt-4">
-            <div class="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white max-h-[30rem] overflow-auto gap-4">
-                <div class="m-2">
-                    @include('components.hr_card')
-                </div>
-                <div class="m-2">
-                    @include('components.hr_card')
-                </div>
-                <div class="m-2">
-                    @include('components.hr_card')
-                </div>
-                <div class="m-2">
-                    @include('components.hr_card')
-                </div>
-                <div class="m-2">
-                    @include('components.hr_card')
-                </div>
-                <div class="m-2">
-                    @include('components.hr_card')
-                </div>
+            <div class="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white max-h-auto overflow-auto gap-4">
+                @foreach ($bids as $bid)
+                    @if ($bid->auction->auction_status == "ongoing")  
+                    <div class="m-2">
+                        <div class="hr_card">
+                            <a href="{{route('product-page', ['id' => $bid->auction->product->id])}}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                @if ($bid->auction->product->images->count() > 0)
+                                <img class="rounded-t-lg h-52 w-64 object-cover" src="{{ asset('storage/' . $bid->auction->product->images->first()->path) }}" alt="" />
+                                @endif
+                                <div class="flex flex-col justify-between p-4 leading-normal w-full items-center">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$bid->auction->product->name}}</h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$bid->auction->product->description}}</p>
+                                    <div class="w-full flex justify-evenly items-center h-full">
+                                        <p class="mb-3 font-normal text-lg text-blue-500 dark:text-blue-500">{{$bid->auction->product->starting_price}}</p>
+                                        <p class="mb-3 font-normal text-lg text-red-800">{{\Carbon\Carbon::parse(($bid->auction)->start_time)->diffForHumans() }}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>                    
+                    </div>
+                    @endif                    
+                @endforeach
             </div>
 
         </div>
         <div id="tabs-2" class="tab-content hidden w-full flex flex-warp flex-col gap-2  mt-4">
             <div class="grid grid-cols-2">
-                <div class="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white max-h-[30rem] overflow-auto gap-4">
+                <div class="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white max-h-auto overflow-auto gap-4">
+                    @foreach ($bids as $bid)
+                    @if ($bid->auction->auction_status == "ended" && $bid->auction->payment_status == "unpaid" && $bid->auction->winner_id == Auth::id())  
                     <div class="m-2">
-                        @include('components.hr_card')
+                        <div class="hr_card">
+                            <a href="{{route('product-page', ['id' => $bid->auction->product->id])}}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                @if ($bid->auction->product->images->count() > 0)
+                                <img class="rounded-t-lg h-52 w-64 object-cover" src="{{ asset('storage/' . $bid->auction->product->images->first()->path) }}" alt="" />
+                                @endif
+                                <div class="flex flex-col justify-between p-4 leading-normal w-full items-center">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$bid->auction->product->name}}</h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$bid->auction->product->description}}</p>
+                                    <div class="w-full flex justify-evenly items-center h-full">
+                                        <p class="mb-3 font-normal text-lg text-blue-500 dark:text-blue-500">{{$bid->auction->product->starting_price}}</p>
+                                        <p class="mb-3 font-normal text-lg text-red-800">{{\Carbon\Carbon::parse(($bid->auction)->start_time)->diffForHumans() }}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>                    
                     </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
-                    <div class="m-2">
-                        @include('components.hr_card')
-                    </div>
+                    @endif                    
+                @endforeach
                 </div>
                 <div class="flex items-center justify-center">
                     <div class="flex flex-col items-center justify-center w-[22rem] h-[18rem] rounded-md border border-blue-500 bg-white-100 drop-shadow-lg">
