5f6739bb0fdd4f48536bbe9d21c2b2932708614d
EzraArya <ezrawijaya10@gmail.com>
2024-06-05 18:08:48 +0700

    feat: Update routes/web.php with new controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuctionController;
+use App\Http\Controllers\BidController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,42 +24,25 @@ Route::group(['middleware' => 'guest'], function(){
     Route::post('/login', [UserController::class, 'LoginPost'])->name('login');
     Route::get('/register',[UserController::class, 'Register'])->name('register-page');
     Route::post('/register',[UserController::class, 'RegisterPost'])->name('register');
-    // Route::get('/login', function(){
-    //     return view('sections.login');
-    // })->name('login-page');
-    // Route::get('/register', function(){
-    //     return view('sections.register');
-    // })->name('register-page');
     Route::get('/forgot-password', function(){
         return view('sections.forgot-password');
     })->name('forgot-password');
+    Route::post('/forgot-edit', [UserController::class, 'resetPassword'])->name('forgot-edit');
     Route::get('/forgot-success', function(){
         return view('sections.forgot-success');
     })->name('forgot-success');
-    Route::get('/forgot-edit', function(){
-        return view('sections.forgot-edit');
-    })->name('forgot-edit');
 });
 
 Route::group(['middleware' => 'auth'], function(){
-    Route::post('/auctions/{auction}/bids', [AuctionController::class, 'placeBid'])->name('bid-store');
+    Route::post('/auctions/{auction}/bids', [BidController::class, 'placeBid'])->name('bid-store');
+    Route::post('/auctions/{auction}/buyout', [AuctionController::class, 'buyoutProduct'])->name('buyout-store');
     Route::get('profile-page', [UserController::class, 'index'])->name('profile-page');
     Route::post('/store-address', [UserController::class, 'storeAddress'])->name('store-address');
-    Route::get('/transaction-history', function(){
-        return view('sections.Profiles.transaction-history');
-    })->name('transaction-history');
+    Route::get('/transaction-history', [TransactionController::class, 'index'])->name('transaction-history');
     Route::get('/seller-home', [ProductController::class, 'indexSeller'])->name('seller-home');
-    // Route::get('/seller-home', function(){
-    //     return view('sections.Sellers.home');
-    // })->name('seller-home');
-    // Route::get('/product-listing', function(){
-    //     return view('sections.Sellers.product-listing');
-    // })->name('create-product');
     Route::get('/create-product', [CategoryController::class, 'create'])->name('create-product');
     Route::post('/store-product', [ProductController::class, 'createProduct'])->name('store-product');
-    Route::get('/cart', function(){
-        return view('sections.Transactions.cart');
-    })->name('cart');
+    Route::get('/cart', [CartController::class, 'index'])->name('cart');
     Route::get('/payment-method', function(){
         return view('sections.Transactions.payment');
     })->name('payment-page');
