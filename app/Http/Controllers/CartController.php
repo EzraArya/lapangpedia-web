b5cad57686cf98a12117046ea2ef2a2cd9fa02e5
EzraArya <ezrawijaya10@gmail.com>
2024-06-06 11:02:22 +0700

    refactor: Optimize CartController@index to fetch latest bids for each auction

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,11 +6,20 @@ use App\Models\Auction;
 use App\Models\Bid;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
     public function index(){
-        $bids = Bid::where('user_id', Auth::id())->with('auction.product')->get();
+        $bids = DB::table('bids')
+                    ->select('auction_id', DB::raw('MAX(id) as id'))
+                    ->where('user_id', Auth::id())
+                    ->groupBy('auction_id')
+                    ->get()
+                    ->map(function ($bid) {
+                        return Bid::where('id', $bid->id)->with('auction.product')->first();
+                    });
+    
         return view('sections.Transactions.cart', ["bids" => $bids]);
     }
 }
