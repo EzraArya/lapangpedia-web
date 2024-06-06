07f2ebe29a0fc2f3108cb43d4eeeac15dccffe90
EzraArya <ezrawijaya10@gmail.com>
2024-06-06 11:01:43 +0700

    refactor: Update search bar in search_bar.blade.php to include category selection and product name input

--- resources/views/components/search_bar.blade.php
+++ resources/views/components/search_bar.blade.php
@@ -1,14 +1,12 @@
 <div class="search_bar">
-    <form class="d-flex gap-1" role="search">
-            <select name="country" class="border border-grey-100 focus:border-black rounded-md h-10 w-28 indent-1 font-montserrat text-sm" placeholder="example@gmail.com">
-                <option value="" selected>Categories</option>
-                <option value="">Figure</option>
-                <option value="">Clothes</option>
-                <option value="">Car</option>
-                <option value="">Watches</option>
-                <option value="">Bike</option>
+    <form class="d-flex gap-1" role="search" action="{{route('search')}}" method="GET">
+        @csrf
+            <select name="category_id" class="border border-grey-100 focus:border-black rounded-md h-10 w-28 indent-1 font-montserrat text-sm" placeholder="example@gmail.com">
+                @foreach($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
             </select>
-        <input class="flex-grow w-[500px] border border-1 border-grey-100 text-center" type="search" placeholder="Search" aria-label="Search">
+        <input class="flex-grow w-[500px] border border-1 border-grey-100 text-center" type="search" placeholder="Search" aria-label="Search" name="product_name">
         <button class="btn border border-blue-500 hover:bg-blue-500 hover:text-white-100    " type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
     </form>
 </div>
