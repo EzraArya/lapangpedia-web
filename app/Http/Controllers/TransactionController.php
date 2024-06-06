b6bf0baf06b66c959bc3409711106702f3f6f5e6
EzraArya <ezrawijaya10@gmail.com>
2024-06-06 09:40:22 +0700

    feat: Add showProductTransaction and transactionPayment methods to TransactionController

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Auction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class TransactionController extends Controller
 {
@@ -14,5 +15,26 @@ class TransactionController extends Controller
         return view('sections.Profiles.transaction-history', ['auctions' => $auctions]);
     }
 
+    public function showProductTransaction($id){
+        $auction = Auction::find($id);
+
+        if (!$auction) {
+            // Handle the case where the product was not found
+            abort(404);
+        }
+
+        return view('sections.Transactions.payment', ['auction' => $auction]);
+    }
+
+    public function transactionPayment(Auction $auction){
+        if ($auction->payment_status == "unpaid") {
+            $auction->payment_status = 'paid';
+            $auction->save();
+    
+            return redirect()->route('payment-success');
+        }
+    
+        return redirect()->back();
+    }
     
 }
