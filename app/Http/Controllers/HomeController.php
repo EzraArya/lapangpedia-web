4bc411d68cfd069adf7103e8337265eb9cdffa5e
EzraArya <ezrawijaya10@gmail.com>
2024-06-06 11:02:57 +0700

    refactor: Add search bar with categories to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Auction;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,5 +13,11 @@ class HomeController extends Controller
     {
         $auctions = Auction::where('end_time', '>', DB::raw('now()'))->get();
         return view('sections.home', ['auctions' => $auctions]);
+    }
+
+    public function showSearchBar()
+    {
+        $categories = Category::all();
+        return view('components.search_bar', ['categories' => $categories]);
     }
 }
