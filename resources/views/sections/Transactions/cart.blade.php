d3ea097116134ebc6e077f78385ccb253713dc41
EzraArya <ezrawijaya10@gmail.com>
2024-06-06 09:40:34 +0700

    feat: Update cart.blade.php to display product details and add checkout functionality

--- resources/views/sections/Transactions/cart.blade.php
+++ resources/views/sections/Transactions/cart.blade.php
@@ -44,31 +44,38 @@
 
         </div>
         <div id="tabs-2" class="tab-content hidden w-full flex flex-warp flex-col gap-2  mt-4">
-            <div class="grid grid-cols-2">
                 <div class="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white max-h-auto overflow-auto gap-4">
                     @foreach ($bids as $bid)
                     @if ($bid->auction->auction_status == "ended" && $bid->auction->payment_status == "unpaid" && $bid->auction->winner_id == Auth::id())  
                     <div class="m-2">
                         <div class="hr_card">
-                            <a href="{{route('product-page', ['id' => $bid->auction->product->id])}}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                                 @if ($bid->auction->product->images->count() > 0)
                                 <img class="rounded-t-lg h-52 w-64 object-cover" src="{{ asset('storage/' . $bid->auction->product->images->first()->path) }}" alt="" />
                                 @endif
-                                <div class="flex flex-col justify-between p-4 leading-normal w-full items-center">
+                                <div class="flex flex-col justify-between p-4 leading-normal w-full h-full items-center">
                                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$bid->auction->product->name}}</h5>
                                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$bid->auction->product->description}}</p>
                                     <div class="w-full flex justify-evenly items-center h-full">
-                                        <p class="mb-3 font-normal text-lg text-blue-500 dark:text-blue-500">{{$bid->auction->product->starting_price}}</p>
+                                        <p class="mb-3 font-normal text-lg text-blue-500 dark:text-blue-500">{{($bid->auction)->starting_price}}</p>
                                         <p class="mb-3 font-normal text-lg text-red-800">{{\Carbon\Carbon::parse(($bid->auction)->start_time)->diffForHumans() }}</p>
                                     </div>
+                                    <div class="w-full flex justify-evenly items-center h-full">
+                                        <a href="{{route('product-page', ['id' => $bid->auction->product->id])}}" class="font-montserrat font-medium text-sm border rounded-md border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white-100 w-24 transition duration-150 focus:border-black h-8 justify-center text-center items-center flex">
+                                            View
+                                        </a>
+                                        <a href="{{route('payment-page', ['id' => $bid->auction->product->id])}}" class="font-montserrat font-medium text-sm border rounded-md border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white-100 w-24 transition duration-150 focus:border-black h-8 justify-center text-center items-center flex">
+                                            Checkout
+                                        </a>
+                                    </div>
                                 </div>
-                            </a>
+                            </div>
                         </div>                    
                     </div>
                     @endif                    
                 @endforeach
                 </div>
-                <div class="flex items-center justify-center">
+                {{-- <div class="flex items-center justify-center">
                     <div class="flex flex-col items-center justify-center w-[22rem] h-[18rem] rounded-md border border-blue-500 bg-white-100 drop-shadow-lg">
                         <div class="m-4 w-full h-full flex flex-col gap-4 p-4 font-montserrat">
                             <h1 class="font-montserrat text-md font-semibold">Order Summary</h1>
@@ -88,11 +95,10 @@
                                 <h1>Order Total</h1>
                                 <h1>xxx</h1>
                             </div>
-                            <x-button text="Checkout" :link="route('payment-page')" class="rounded-md bg-blue-500 font-montserrat font-bold w-full text-white-100 hover:bg-white-100 hover:border-blue-500 text-lg border-blue-500 border-2 duration-150 h-11 hover:text-black mb-4"></x-button>
+                            <x-button text="Checkout" :link="route('payment-page', )" class="rounded-md bg-blue-500 font-montserrat font-bold w-full text-white-100 hover:bg-white-100 hover:border-blue-500 text-lg border-blue-500 border-2 duration-150 h-11 hover:text-black mb-4"></x-button>
                         </div>
                     </div>
-                </div>
-            </div>
+                </div> --}}
         </div>
     </div>
 </section>
