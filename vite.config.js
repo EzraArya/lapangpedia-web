b2772c13b393040feeccb38c35294f8e015fe66e
EzraArya <ezrawijaya10@gmail.com>
2024-06-07 21:48:49 +0700

    Changed vite.config.js configuration

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: 'https://lelangpedia.vercel.app/',
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
