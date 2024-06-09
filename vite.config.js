a460c66159bdc59d15f30a6664306fc4e36832ac
EzraArya <ezrawijaya10@gmail.com>
2024-06-09 23:14:40 +0700

    refactor: Remove base URL from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'https://lelangpedia.vercel.app/',
     plugins: [
         laravel({
             input: [
